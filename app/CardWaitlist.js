5b1bae7e571b4218bb0735e92793ad5391b591cc
Emmanuel Agüero <54730752+EmmanuelAR@users.noreply.github.com>
2025-07-21 22:26:42 -0600

    #61 Refactor CardWaitlist to use axios for API requests and integrate CAVOS_CORE_API and CAVOS_CORE_TOKEN for waitlist submissions.

--- app/CardWaitlist.js
+++ app/CardWaitlist.js
@@ -16,8 +16,10 @@ import {
     ActivityIndicator
 } from 'react-native';
 import { supabase } from '../lib/supabaseClient';
-
 const { width, height } = Dimensions.get('window');
+import { CAVOS_CORE_API, CAVOS_CORE_TOKEN } from '../lib/constants';
+import axios from 'axios';
+
 
 const scale = size => width / 375 * size;
 const verticalScale = size => height / 812 * size;
@@ -151,14 +153,22 @@ export default function CardWaitlist() {
         
         setIsSubmitting(true);
         try {
-            const {error} = await supabase.from('card_waitlist').insert([
+            const response = await axios.post(
+                CAVOS_CORE_API + 'v1/cardWaitlist',
                 {
                     email: email.toLowerCase(),
                     country: country,
+                },
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        Authorization: `Bearer ${CAVOS_CORE_TOKEN}`,
+                    },
                 }
-            ]);
-            if (error) {
-                console.error('Error joining waitlist:', error);
+            );
+            
+            if (response.status!=201) {
+                console.error('Error joining waitlist:', response.message);
                 Alert.alert('Error', 'Could not join the waitlist. Please try again.');
                 return;
             }
